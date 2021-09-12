7daceb595ac7c5578076b702e38623979a0cecd7
Ivan Demidov <ivan.demidov00@mail.ru>
2021-09-12 19:42:54 +0300

    aspx service

---
+++ Web/WebService.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Web
+{
+    /// <summary>
+    /// Сводное описание для WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Привет всем!";
+        }
+        [WebMethod]
+        public List<Int32> go(Int32 n)
+        {
+            List<Int32> list = new List<Int32>();
+            for (Int32 i = 0; i < n; i++) 
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+    }
+}
